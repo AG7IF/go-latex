749e6e593d96ed634e93db89dda9a630a7abd0fa
Robert Hawk <robert.hawk@gmail.com>
2025-10-14 02:31:36 -0600

    Savepoint commit

--- stddocs/article.go
+++ stddocs/article.go
@@ -1,1 +1,48 @@
 package stddocs
+
+import (
+	"github.com/ag7if/go-files"
+
+	"github.com/ag7if/go-latex"
+)
+
+type Article struct {
+}
+
+func NewArticle() *Article {
+	panic("implement me")
+}
+
+func (a *Article) LaTeX() string {
+	latex := `\documentclass{article}`
+}
+
+func (a *Article) AssetDir() *files.Directory {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a *Article) Assets() []files.File {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a *Article) BuildEngine() latex.BuildEngine {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a *Article) IncludePackage(pkg latex.Package, args ...string) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a *Article) WriteClass() error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a *Article) WritePackages() error {
+	//TODO implement me
+	panic("implement me")
+}
